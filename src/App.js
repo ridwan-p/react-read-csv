05f8980ce0723049ba410019d41aed67e10e3244
Ridwan Pamungkas <ridwan@jogjacamp.co.id>
2019-10-25 16:11:01 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Papa from 'papaparse'
+import {makePhoneContry} from "./common/untils"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {contacts:[]}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    const vm = this
+    Papa.parse(e.target.files[0], {
+      header: true,
+      complete: function(results) {
+        // console.log(this)
+        vm.setState({contacts:results.data})
+      }
+    });
+  }
+
+  render() {
+    const {contacts} = this.state
+    return  (
+      <div className="App">
+        <input type="file" name="file" onChange={this.handleChange} />
+
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Number</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              contacts.map((item, i) =>
+                <tr key={i}>
+                  <td>{i+1}</td>
+                  <td>{item['Name']}</td>
+                  <td>{makePhoneContry(item['Phone 1 - Value'])}</td>
+                  <td><button>{item['status'] || 'X'}</button></td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/common/untils.js
@@ -0,0 +1,14 @@
+const PHONE_COUNTRY = {
+    'id': 62
+}
+
+/**
+ * it make phone number stardart country
+ * @param  {String} phone
+ * @param  {String} country
+ * @return {String}
+ */
+export const makePhoneContry = (phone, country = 'id') => {
+    let newPhone = phone.replace(/-/g, "")
+    return PHONE_COUNTRY[country] + ( (newPhone.substr(0,1) === "0" )? newPhone.substr(1) : newPhone )
+}
