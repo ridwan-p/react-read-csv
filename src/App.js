7270bd9813b89485a510eeca9938c9797fd883ec
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-10-25 23:05:17 +0700

    specific header csv

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import {makePhoneCountry} from "./common/untils"
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {data:[], key: [], file: null}
+    this.state = {data:[], keys: [], file: null}
     this.handleChange = this.handleChange.bind(this)
     this.handleRender = this.handleRender.bind(this)
   }
 
   handleChange(e){
     const name = e.target.name
-    const value = e.target.type === 'file' ? e.target.files[0] : e.target.value
+    const value = e.target.type === 'file' ? e.target.files[0] : e.target.value.replace(/, /g, ',').split(',')
     this.setState({ [name]: value })
   }
 
@@ -31,32 +31,30 @@ class App extends Component {
   }
 
   render() {
-    const {data} = this.state
+    const {data, keys} = this.state
     return  (
       <div className="App">
         <input type="file" name="file" onChange={this.handleChange} />
-        <input type="text" placeholder="name,phone,key"/>
+        <input type="text" name="keys" onChange={this.handleChange} placeholder="name,phone,key"/>
         <button onClick={this.handleRender}>Submit</button>
 
         <table>
           <thead>
             <tr>
-              <th>No</th>
-              <th>Name</th>
-              <th>Number</th>
-              <th>Status</th>
+              {
+                keys.map(key => <th key={key}>{key}</th>)
+              }
+            
             </tr>
           </thead>
           <tbody>
             {
               data.map((item, i) => {
-                const phone = makePhoneCountry(item['Phone 1 - Value'])
                 return (
                   <tr key={i}>
-                    <td>{i+1}</td>
-                    <td> <a href={`https://wa.me/${phone.replace(/\D/g, "")}?text=Saya%20tertarik%20untuk%20membeli%20mobil%20Anda`} target="_blank" rel="noopener noreferrer">{item['Name']}</a></td>
-                    <td>{phone}</td>
-                    <td><button>{item['status'] || 'X'}</button></td>
+                    {
+                      keys.map(key => <td key={key}>{key === 'phone' ? makePhoneCountry(item[key]) : item[key]}</td>)
+                    }
                   </tr>
                 )
               })
